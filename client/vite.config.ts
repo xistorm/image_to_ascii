40aa5ce69ebf3c783379044ec4c3efff632086b0
xistorm <xis402@gmail.com>
2025-01-28 23:44:49 +0300

    config env

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	css: {
+		modules: {
+			localsConvention: 'camelCaseOnly',
+		},
+	},
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+			'@pages': path.resolve('./src/pages'),
+			'@features': path.resolve('./src/features'),
+			'@assets': path.resolve('./src/assets'),
+			'@shared': path.resolve('./src/shared'),
+			'@services': path.resolve('./src/services'),
+		},
+	},
+});
